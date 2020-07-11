e58d78e625baba9f503ffac30eebd1b44d3e5d3b
s99113266 <41054903+s99113266@users.noreply.github.com>
2020-07-11 21:08:03 +0800

    忘記了

---
+++ 2020_Unity_Run_Cool/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScript : MonoBehaviour
+{
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// 切換場景
+    /// </summary>
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("RunCoolGame");
+    }
+
+
+    /// <summary>
+    /// 延長呼叫方法
+    /// </summary>
+    /// <param name="MothodName">方法名稱</param>
+    public void DelayMothod(string MothodName)
+    {
+        Invoke(MothodName, 0.8f);
+    }
+}
